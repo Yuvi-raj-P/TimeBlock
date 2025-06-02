ae7ab4282fe425f97bfa973c9b15d96be0e58c4a
Yuvi-raj-P <yuvilds@gmail.com>
2025-06-01 23:30:21 -0400

    Initial Commit

---
+++ TimeBlock/TimeBlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeBlockApp.swift
+//  TimeBlock
+//
+//  Created by Yuvraj P on 6/1/25.
+//
+
+import SwiftUI
+
+@main
+struct TimeBlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimeBlockTests/TimeBlockTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimeBlockTests.swift
+//  TimeBlockTests
+//
+//  Created by Yuvraj P on 6/1/25.
+//
+
+import Testing
+@testable import TimeBlock
+
+struct TimeBlockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
